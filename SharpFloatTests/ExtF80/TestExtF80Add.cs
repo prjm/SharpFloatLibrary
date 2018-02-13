ec15716836089a1d75dc4715a891cf00365a5966
prjm <prjm@users.noreply.github.com>
2018-02-14 00:25:26 +0100

    bugfix, changed test

--- SharpFloatTests/ExtF80/TestExtF80Add.cs
+++ SharpFloatTests/ExtF80/TestExtF80Add.cs
@@ -12,9 +12,9 @@ namespace SharpFloatTests.ExtF80 {
             Assert.EqualUShort(0, zero.signExp);
             Assert.EqualULong(0, zero.signif);
 
-            var caseOne = new E(0x3F80, 0xE00000000000010) + new E(0xBBFF, 0xFFFEFFFFBFFFFFFF);
-            Assert.EqualUShort(0x3F80, caseOne.signExp);
-            Assert.EqualULong(0xE000000000000010, caseOne.signif);
+            var caseOne = new E(0x4002, 0x8000400010000000) + new E(0xC002, 0x8040000FFFFFFFFF);
+            Assert.EqualUShort(0xBFF8, caseOne.signExp);
+            Assert.EqualULong(0xFF003FBFFFFFFC00, caseOne.signif);
         }
 
     }
