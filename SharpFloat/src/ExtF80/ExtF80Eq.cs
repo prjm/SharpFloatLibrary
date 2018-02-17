f75271a5656772d2588d274064a49169a5dbf353
prjm <prjm@users.noreply.github.com>
2018-02-17 22:01:16 +0100

    moved files

---
+++ SharpFloat/src/ExtF80/ExtF80Eq.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SharpFloat.Globals;
+
+namespace SharpFloat.ExtF80 {
+
+    public partial struct ExtF80 : IEquatable<ExtF80> {
+
+        public static bool operator ==(ExtF80 l, ExtF80 r) {
+            if (IsNaNExtF80UI(l.signExp, l.signif) || IsNaNExtF80UI(r.signExp, r.signif)) {
+
+                if (IsSigNaNExtF80UI(l.signExp, l.signif) || IsSigNaNExtF80UI(r.signExp, r.signif)) {
+                    Settings.Raise(ExceptionFlags.Invalid);
+                }
+
+                return false;
+            }
+
+            return //
+                (l.signif == r.signif)
+                && ((l.signExp == r.signExp) || (l.signif == 0 && 0 != ((l.signExp | r.signExp) & 0x7FFF)));
+        }
+
+        /// <summary>
+        ///     check for equality
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj) {
+            if (obj is ExtF80 e)
+                return e == this;
+            return false;
+        }
+
+        public bool Equals(ExtF80 other)
+            => this == other;
+
+        public override int GetHashCode()
+            => (signExp.GetHashCode() * 397) ^ signif.GetHashCode();
+    }
+
+}
---
+++ SharpFloat/src/ExtF80/ExtF80Neq.cs
@@ -0,0 +1,24 @@
+﻿using SharpFloat.Globals;
+
+namespace SharpFloat.ExtF80 {
+
+    public partial struct ExtF80 {
+
+        public static bool operator !=(ExtF80 l, ExtF80 r) {
+            if (IsNaNExtF80UI(l.signExp, l.signif) || IsNaNExtF80UI(r.signExp, r.signif)) {
+
+                if (IsSigNaNExtF80UI(l.signExp, l.signif) || IsSigNaNExtF80UI(r.signExp, r.signif)) {
+                    Settings.Raise(ExceptionFlags.Invalid);
+                }
+
+                return false;
+            }
+
+            return //
+                (l.signif != r.signif)
+                || ((l.signExp != r.signExp) && (l.signif != 0 && 0 == ((l.signExp | r.signExp) & 0x7FFF)));
+        }
+
+
+    }
+}
