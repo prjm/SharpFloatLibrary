67a0a6e8ff23d597813f75879e2a7f56cf6138d9
prjm <prjm@users.noreply.github.com>
2018-02-25 00:28:59 +0100

    fixed test case

--- SharpFloatTests/src/Helpers/TestUInt128Mul64ByShifted32To128.cs
+++ SharpFloatTests/src/Helpers/TestUInt128Mul64ByShifted32To128.cs
@@ -10,13 +10,13 @@ namespace SharpFloatTests.src.Helpers {
             var b = 0x111111U;
             var r = UInt128.Mul64ByShifted32To128(a, b);
             Assert.EqualULong(0x332FC96300000000, r.v0);
-            Assert.EqualULong(0x332FC963, r.v64);
+            Assert.EqualULong(0x000369d033333333, r.v64);
 
             a = 0x333333311111111UL;
             b = 0x111222U;
             r = UInt128.Mul64ByShifted32To128(a, b);
             Assert.EqualULong(0x9998764200000000, r.v0);
-            Assert.EqualULong(0x6CC962C6, r.v64);
+            Assert.EqualULong(0x000036a06cca861d, r.v64);
         }
     }
 }
